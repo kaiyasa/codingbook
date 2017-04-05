8febbdd4da5175f3f715d90ab62d301d27abca2c
Dan Miner <dan.miner@dish.com>
2017-04-05 13:47:30 -0600

    progress to chapter 2 of the LLVM tutorial

--- llvm/kaleidoscope/cpp/main.mk
+++ llvm/kaleidoscope/cpp/main.mk
@@ -2,7 +2,7 @@
 TARGET       := kcc
 TARGET_DIR   := build/dist
 
-CXXFLAGS     := -std=c++11 -pthread -g
+CXXFLAGS     := -std=c++11 -g -I/usr/include/llvm-3.8
 SOURCES      := $(shell find src -name '*.cpp')
 TGT_LDFLAGS  :=
 TGT_LDLIBS   :=
